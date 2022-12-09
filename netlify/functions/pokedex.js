8b2061c4593c6de13e1a5c8f83519a1fc18c9cb1
David DiBenedetto <david@njweb.solutions>
2022-12-09 11:13:25 -0500

    Finished product
    Push finished product to production

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,12 +1,24 @@
 // mod.cjs
-// eslint-disable-next-line no-shadow
+// eslint-disable-next-line no-shadow, import/no-extraneous-dependencies
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-exports.handler = async function () {
-  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto';
+const chalk = require('chalk');
+const { DateTime } = require('luxon');
+exports.handler = async function (event, context) {
+  const eventBody = JSON.parse(event.body);
+  const date = DateTime.now();
+  const color = eventBody.region === 'kanto' ? chalk.blue : chalk.green;
+
+  console.log(color(`${date}: Fetching data from PokeAPI`));
+  console.log(color(`\teventBody.region: ${eventBody.region}`));
+
+  const POKE_API = `https://pokeapi.co/api/v2/pokedex/${eventBody.region}`;
+
   const response = await fetch(POKE_API);
   const data = await response.json();
   return {
     statusCode: 200,
-    body: JSON.stringify(data)
-  }
-}
+    body: JSON.stringify({
+      pokemon: data.pokemon_entries,
+    }),
+  };
+};
